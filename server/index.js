2325b59894e63e0f3c6c238029bcca522192e5a1
Nicholas Igunbor <nicholas.igunbor@decagon.dev>
2023-01-23 18:23:07 +0100

    changed the port numbers

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import {dirname, join} from 'path';
+import { fileURLToPath } from "url";
 
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
+// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
@@ -17,6 +22,15 @@ app.use('/api/v1/dalle', dalleRoutes);
 app.get('/', async(req, res) => {
   res.send('Hello from dali');
 });
+
+if (process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static("../client/dist"));
+}
+
+app.get('/*', function(req, res) {
+    res.sendFile(join(__dirname, "../client/dist/index.html"));
+  });
 
 const startServer = async () => {
     try{
